1f55a7ccddb3b353d2f9876e4753dd8877854a72
dorryldee <dorrymbula2022@gmail.com>
2025-11-30 17:37:34 +0300

    updated files

--- defensive_driving_school/myapp/views.py
+++ defensive_driving_school/myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect,render
 from .models import Student, Register
 from .forms import StudentForm, RegisterForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required,user_passes_test
 
 # Create your views here.
 def home(request):
@@ -15,7 +15,7 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('homepage')
+            return redirect('loginpage')
         # If form is invalid fall through to re-render with errors
     else:
         form = RegisterForm()
@@ -24,18 +24,29 @@ def register(request):
     
 def login(request):
     return render(request, 'login.html')
+
+def is_admin(user):
+    return user.is_authenticated and user.is_staff
+
+@login_required
+@user_passes_test(is_admin)
 def sidebar(request):
     return render(request,'sidebar.html')
+
+@login_required
+@user_passes_test(is_admin)
 def studentform(request):
     student=Student.objects.all()
     return render(request, "studentform.html", {'student':student})
+
 @login_required
-
-
+@user_passes_test(is_admin)
 def studentlist(request):
     student=Student.objects.all()
     return render(request, "studentlist.html", {'student':student})
 
+@login_required
+@user_passes_test(is_admin)
 def create_student(request):
     if request.method == "POST":
         form=StudentForm(request.POST)
@@ -48,7 +59,8 @@ def create_student(request):
 
     return render(request, "studentform.html", {'form':form})
 
-
+@login_required
+@user_passes_test(is_admin)
 def update_student(request,id):
     student=Student.objects.get(id=id)
     form=StudentForm(instance=student)
@@ -61,6 +73,8 @@ def update_student(request,id):
 
     return render(request,"studentform.html",{'form':form})
 #delete a record
+@login_required
+@user_passes_test(is_admin)
 def delete_student(request,id):
     student=Student.objects.get(id=id)
     student.delete()
