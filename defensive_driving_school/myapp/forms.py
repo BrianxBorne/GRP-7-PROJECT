6928e9647c1ece3ac760fcdbb8ffbadd6fb80d81
FelixM-ai <mutugifelix63@gmail.com>
2025-11-30 13:25:15 +0300

    Added bootstrap styling to register form

---
+++ defensive_driving_school/myapp/forms.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model=User;
+        fields=['username','first_name','email','password1','password2']
--- defensive_driving_school/myapp/views.py
+++ defensive_driving_school/myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from urllib import request
+from django.shortcuts import redirect, render
+from .forms import RegisterForm
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -8,7 +9,15 @@ def about(request):
     return render(request, 'about.html')
 
 def register(request):
-    return render(request, 'register.html')
+     if request.method == "POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #redirect to home page
+            return redirect('home')
+     else:
+        form=RegisterForm()
+     return render(request, 'register.html', {'form':form})
     
 def login(request):
     return render(request, 'login.html')
